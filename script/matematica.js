f6d3b8ffbcd9497ccf1878dc0cab947f3b9df0ce
AdamMillerCooper24 <00001133450222sp@al.educacao.sp.gov.br>
2024-11-23 14:51:01 -0300

    adicionando pastas

---
+++ script/matematica.js
@@ -0,0 +1,82 @@
+const data = [
+
+    {
+    title: "Operações Básicas",
+    description:
+    "Aprenda as quatro operações centrais da matemática (adição, subtração, multiplicação e divisão) de maneira fácil e didática, trabalhando suas nomeclaturas e operações inversas. Você tem acesso a vídeos, artigos e exercícios.",
+    bottom: "Não sei"
+    },
+    {
+        title: "Expressões Númericas",
+        description:
+        "Aprenda sobre a ordem de resoluções preestabelecidas em sequências de operações, abordando as quatro operações básicas da matemática. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+            title: "Trigonometria",
+            description:
+            "Compreenda as relações entre os lados do triângulo e seus ângulos, trabalhando seno, cosseno e tangente. Você tem acesso a vídeos, artigos e exercícios.",
+            bottom: "Não sei"
+    },
+    {
+                title: "Teorema de Pitágoras",
+                description:
+                "O Teorema de Pitágoras determina a proporção dos catetos de um triângulo reto a sua hipotenusa, trabalhando raízes e equação. Você tem acesso a vídeos, artigos e exercícios.",
+                bottom: "Não sei"
+    },
+    {
+        title: "Equações de Primeiro Grau",
+        description:
+        "Trabalhe como identificar, solucionar e aplicar equações de primeiro  grau. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Funções",
+        description:
+        "Entenda funções como a relação que os elementos de dois conjuntos tem entre si, trabalhando domínio, imagem, contra imagem e gráficos. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Frações",
+        description:
+        "A fração é a representação da divisão entre dois números, entenda suas nomeclaturas e propriedades. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Área de figuras planas",
+        description:
+        "Trabalhe como descobrir a área das formas mais recorrentes da matemática. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Gráficos de funções",
+        description:
+        "Compreenda as relações dos coeficientes das funções para a construção de gráficos, com funções de primeiro e segundo grau e função exponencial. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Razão e Proporção",
+        description:
+        "Trabalhe grandezas e suas proporções, abordando como descobrir proporções, grandezas diretamente e inversamente proporcionais e aplicação da regra de três . Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Círculo Trigonometrico",
+        description:
+        "Aprenda a montar e interpretar um círculo trigonometrico, compreender as representações das razões trigonometricas de seno, cosseno e tangente, além de seus cálculos. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Círculo e Circunferência",
+        description:
+        "Compreenda os conceitos de círculo, circunferência, raio, diâmetro e trabalhe com o Pi. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+    {
+        title: "Estatística",
+        description:
+        "Saiba como levantar dados de fenômenos ou populações, relacionando fatos e números. Você tem acesso a vídeos, artigos e exercícios.",
+        bottom: "Não sei"
+    },
+
+    ]
